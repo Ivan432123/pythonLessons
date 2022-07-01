cd100c34457268ab2e8700b01f3fa9d758e8da54
Ivan432123 <96534428+Ivan432123@users.noreply.github.com>
2022-07-01 21:47:47 +0600

    CСловари и множества

---
+++ dict.py
@@ -0,0 +1,31 @@
+# # список
+
+# family_1 = ["Vasya", "Olga", "Semen", "Dasha", "Vasya"]
+
+# print(family_1)
+
+
+
+# # множества
+
+# family_2 = {"Vasya", "Olga", "Semen", "Dasha"}
+# print(family_2)
+
+
+
+# # Словарь (ключ:значение)
+
+family_3 = {"Папа":"Vasya", "Мама":"Olga", "Сын":"Semen","Дочь":"Dasha"}
+
+print(family_3["Папа"])
+
+
+family_3 = {"Папа":"Vasya", "Мама":"Olga", "Сын":"Semen","Дочь":"Dasha"}
+
+# print(family_3["Папа"])
+
+
+for k, v in family_3.items():
+    
+    
+print(k, v)
